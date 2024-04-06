b22696811cec41507b9445497ea2161ecedf9fd2
TotalBattleBots <noreply@github.com>
2024-04-06 13:27:48 -0400

    add exchanges png

--- Main.py
+++ Main.py
@@ -1,19 +1,35 @@
 import winsound
-import pyautogui as pag  
+import pyautogui as pag
 import time
 from python_imagesearch.imagesearch import imagesearch
- 
+
 # frequency is set to 500Hz
 freq = 500
- 
-# duration is set to 100 milliseconds            
+
+# duration is set to 100 milliseconds
 dur = 500
-              
+
 
 
 while True:
-    pos = imagesearch("Image assets\Exchange.PNG")
-    if pos[0] != -1:                                                                                    
+    pos = imagesearch("exchange1.png")
+    if pos[0] != -1:
         winsound.Beep(freq, dur)
         del pos
+
+    pos = imagesearch("exchange2.png")
+    if pos[0] != -1:
+        winsound.Beep(freq, dur)
+        del pos
+
+    pos = imagesearch("exchange3.png")
+    if pos[0] != -1:
+        winsound.Beep(freq, dur)
+        del pos
+
+    pos = imagesearch("exchange4.png")
+    if pos[0] != -1:
+        winsound.Beep(freq, dur)
+        del pos
+
     time.sleep(0.05)

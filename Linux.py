ca50131878db63cf806e8e0b3638a82f571432e4
TotalBattleBots <noreply@github.com>
2024-05-07 09:28:13 -0400

    Linux.py : use os.system play instead of winsound

--- Linux.py
+++ Linux.py
@@ -4,33 +4,22 @@
 #
 # Works with Python 3.12+
 #
-# Tested successfully on Windows Desktop
 #
 #
-# GO HERE:
-# https://www.python.org/downloads/
-#
-# DOWNLOAD THIS:
-# https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe
-#
-# [x] Install Python3 for all users
-# [x] Add Python3 to your PATH environment
 #
 # DOWNLOAD THIS:
 # https://codeload.github.com/fdd26/Exchange-Beep/zip/refs/heads/main?file=Exchange-Beep-main.zip
 #
 # Extract it
 #
-# OPEN cmd.exe in that folder
+# OPEN bash in that folder
 #
 # TYPE THIS to install Python3 dependencies:
 # pip3 install pyautogui
 # pip3 install python_imagesearch
 #
 # TYPE THIS to run the program
-# python.exe Main.py
-#
-# OR double-click on Main.py
+# python3 Linux.py
 #
 # OPEN exchange.png to test, you should hear some beeps.
 #
@@ -41,8 +30,8 @@
 #
 #####################################################################
 
-import winsound
-import pyautogui as pag
+import os
+#import pyautogui as pag
 import time
 from python_imagesearch.imagesearch import imagesearch
 
@@ -67,31 +56,31 @@ while True:
     # 25% resolution square image search with truncation
     pos = imagesearch("exchange2.png")
     if pos[0] != -1:
-        winsound.Beep(freqMerc, dur)
+        os.system('play -n synth {} sine {}'.format(dur, freqMerc))  # Play if find "exchange"
         del pos
 
     # Truncation search works with side arrows
     pos = imagesearch("exchange3.png")
     if pos[0] != -1:
-        winsound.Beep(freqMerc, dur)
+        os.system('play -n synth {} sine {}'.format(dur, freqMerc))  # Play if find "exchange"
         del pos
 
     # Truncation search works with bottom arrows
     pos = imagesearch("exchange4.png")
     if pos[0] != -1:
-        winsound.Beep(freqMerc, dur)
+        os.system('play -n synth {} sine {}'.format(dur, freqMerc))  # Play if find "exchange"
         del pos
 
     # Truncation search works with bottom arrows
     pos = imagesearch("gold-dm1.png")
     if pos[0] != -1:
-        winsound.Beep(freqGold, dur)
+        os.system('play -n synth {} sine {}'.format(dur, freqGold))  # Play if find "gold-dm"
         del pos
 
     # Truncation search works with bottom arrows
     pos = imagesearch("gold-dm2.png")
     if pos[0] != -1:
-        winsound.Beep(freqGold, dur)
+        os.system('play -n synth {} sine {}'.format(dur, freqGold))  # Play if find "gold-dm"
         del pos
 
     # Sleep a little before looping

c7f54f357871d0e320c6279cf31ec0289892db6a
TotalBattleBots <noreply@github.com>
2024-11-23 19:06:35 -0500

    add mouse-left-click-pyautogui-move-and-click.py

---
+++ mouse-left-click-pyautogui-move-and-click.py
@@ -0,0 +1,17 @@
+#!python3
+
+import winsound
+import pyautogui
+import time
+
+from python_imagesearch.imagesearch import *
+
+time.sleep(2.00)
+
+pyautogui.moveTo(450, 80, duration=0.15)
+
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+
+time.sleep(0.05)
